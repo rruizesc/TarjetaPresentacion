e01dee2dd7b9c855e03d59ccd2c1b7447fb1f156
rruizesc <raulruizvp@gmail.com>
2023-09-20 10:56:01 +0200

    Initial Text

--- app/src/main/java/com/example/tarjetapresentacion/MainActivity.kt
+++ app/src/main/java/com/example/tarjetapresentacion/MainActivity.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tarjetapresentacion.ui.theme.TarjetaPresentacionTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    InitialText(name =  "Jon Raul", positionJob = "prueba" )
+                    InitialImage(name =  "Jon Raul", positionJob = "Android developer" )
 
                 }
             }
@@ -34,20 +43,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun InitialText(name: String, positionJob: String){
-val image= painterResource(id = )
-    Box {
+fun InitialText(name: String, positionJob: String, modifier: Modifier = Modifier) {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.padding(8.dp),
+    ){
         Image(
-            painter = image,
-            contentDescription = null
+            painter = painterResource(R.drawable.android_logo),
+            contentDescription = null,
+            modifier = Modifier
+                .size(120.dp)
+                .padding(bottom = 8.dp)
         )
-
         Text(
             text = name,
-
-            )
+            fontSize = 50.sp,
+            lineHeight = 40.sp,
+            textAlign = TextAlign.Center,
+        )
         Text(
             text = positionJob,
+            fontSize = 36.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+
+        )
+    }
+}
+
+@Composable
+fun InitialImage(name: String, positionJob: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.fondo)
+    Box {
+
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        InitialText(
+            name = name,
+            positionJob = positionJob,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
 
             )
     }
