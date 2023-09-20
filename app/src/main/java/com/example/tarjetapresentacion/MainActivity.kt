b0eb4c97f149d631dec9aac3d87ccd28812c505c
rruizesc <raulruizvp@gmail.com>
2023-09-20 10:03:28 +0200

    Revert "Initial Text"
    This reverts commit eb83de35be43d5e7c5e502231a7a34c58479181b.

--- app/src/main/java/com/example/tarjetapresentacion/MainActivity.kt
+++ app/src/main/java/com/example/tarjetapresentacion/MainActivity.kt
@@ -3,15 +3,12 @@ package com.example.tarjetapresentacion
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tarjetapresentacion.ui.theme.TarjetaPresentacionTheme
 
@@ -25,8 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    InitialText(name =  "Jon Raul", positionJob = "prueba" )
-
+                    Greeting("Android")
                 }
             }
         }
@@ -34,21 +30,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun InitialText(name: String, positionJob: String){
-val image= painterResource(id = )
-    Box {
-        Image(
-            painter = image,
-            contentDescription = null
-        )
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
 
-        Text(
-            text = name,
-
-            )
-        Text(
-            text = positionJob,
-
-            )
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TarjetaPresentacionTheme {
+        Greeting("Android")
     }
 }
